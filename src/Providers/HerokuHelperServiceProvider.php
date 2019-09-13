c87971d222d1b0fbba209aa95309ccbb6083bf85
Harikrushna V. Adiecha <adiechahari@gmail.com>
2019-09-14 02:22:30 +0530

    added basic setup for a laravel package

---
+++ src/Providers/HerokuHelperServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace HK\HerokuHelper\Providers;
+use Illuminate\Support\ServiceProvider;
+class HerokuHelperServiceProvider extends ServiceProvider {
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    	// Need to write registration code here.
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    	// Need to write boot code here.
+    }
+}
